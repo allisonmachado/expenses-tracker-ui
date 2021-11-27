b9532438d47bd2c464620993e6ec24c33a3cfe3c
Allison Machado Gonçalves <aallison93@gmail.com>
2021-11-27 19:28:09 +0000

    add confirmation before logout

--- src/components/layouts/Header/index.js
+++ src/components/layouts/Header/index.js
@@ -1,5 +1,6 @@
 import NavLink from "./NavLink";
 
+import ConfirmationModal from "../../util/ConfirmationModal";
 import { useAuthState } from "../../../hooks/useAuthState";
 
 export default function Header({ authService }) {
@@ -10,22 +11,40 @@ export default function Header({ authService }) {
     auth.signOut();
   }
 
-  const logoutButton = <span className="nav-link" role="button" onClick={logoutUser}>Logout</span>;
-
   return (
     <header>
       <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <a className="navbar-brand" href="/">Cash Tracker</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav mr-auto">
             <NavLink to="/#">Expenses</NavLink>
-            {auth.user ? logoutButton : <NavLink to="/login">Login</NavLink>}
+            {auth.user
+              ? <span
+                  className="nav-link"
+                  role="button"
+                  data-toggle="modal"
+                  data-target="#logoutConfirmationModal">
+                    Logout
+                </span>
+              : <NavLink to="/login">Login</NavLink>}
           </ul>
         </div>
       </nav>
+      <ConfirmationModal
+        id="logoutConfirmationModal"
+        title="Confirmation"
+        action={"Do you want to logout from the application?"}
+        actionHandler={logoutUser} />
     </header>
   );
 }
